361a69490dffc06519ddaaf07384e625063cd76e
shuimqcn <6510833+shuimqcn@users.noreply.github.com>
2021-04-23 14:38:23 +0800

    Update init.py
      # ""前面加上r,路径拷贝过来，不用将\变成\\

--- init.py
+++ init.py
@@ -5,8 +5,8 @@ import bilibili.utils
 # )
 
 bilibili.utils.video_rename(
-    "C:\\Users\\gooin\\AppData\\Local\\Packages\\36699Atelier39.forWin10_pke1vz55rvc1r\\LocalCache\\BilibiliDownload\\55852277"
-)
+    r"C:\Users\users\AppData\Local\Packages\36699Atelier39.forWin10_pke1vz55rvc1r\LocalCache\BilibiliDownload\55852277"
+)  # ""前面加上r,路径拷贝过来，不用将\变成\\
 
 
 
